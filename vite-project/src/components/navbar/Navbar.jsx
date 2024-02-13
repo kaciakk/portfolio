a8c4583ebf34e1809898cf9c7fcbfe1387147469
kaciakk <wlomcj0@gmail.com>
2024-02-13 23:25:04 +0100

    new

--- vite-project/src/components/navbar/Navbar.jsx
+++ vite-project/src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
         <span><a href='#'><motion.img src="/logo.png" alt=""  whileHover={{scale:1.1}} whileTap={{scale:0.9}}/></a></span>
         <div className='social'>
             <a href='https://www.linkedin.com/in/maciej-w%C5%82osek/' target='_blank'><motion.img src="/linkedin-sign.png" alt="" whileHover={{scale:1.1}} whileTap={{scale:0.9}}/></a>
-            <a href='https://github.com/kaciakk' target='_blank'><motion.img src="/github-sign.png" alt=""  whileHover={{scale:1.1}} whileTap={{scale:0.9}} /></a>
+            <a href='https://github.com/kaciakk' target='_blank'><motion.img src="/github-sign.png" alt="a"  whileHover={{scale:1.1}} whileTap={{scale:0.9}} /></a>
         </div>
         </div></div>
   )
